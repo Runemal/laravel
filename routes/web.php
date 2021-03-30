3fbdb2c4fb67fc28314f8f4ac9b7c4df8fd0886b
Runemal <runemal@bk.ru>
2021-03-30 19:46:03 +0300

    Lesson_1_v1

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/news', function () {
-    return view('news');
-});
-Route::get('/contacts', function () {
-    return view('contacts');
-});
+//Route::get('/', function () {
+//    return view('home');
+//});
+//Route::get('/about', function () {
+//    return view('about');
+//});
+//Route::get('/news', function () {
+//    return view('news');
+//});
+//Route::get('/contacts', function () {
+//    return view('contacts');
+//});
